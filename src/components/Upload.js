5470619f9088fd61b4a844d7d0596a429e1375e8
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-03-03 12:40:46 -0500

    changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,58 +1,91 @@
-import * as React from 'react';
-import {
-  Box,
-  Button,
-  Stack,
-  Typography,
-} from '@mui/material';
-import { maxWidth } from '@mui/system';
+import * as React from "react";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { maxWidth } from "@mui/system";
+import { createWorker } from "tesseract.js";
 
 export default function Upload() {
-  const [imageData, setImageData] = React.useState('');
-  const [textData, setTextData] = React.useState('');
-
-
+  const [imageData, setImageData] = React.useState("");
+  const [textData, setTextData] = React.useState("");
+  const convertImageToText = async (file) => {
+    const worker = await createWorker({
+      logger: (m) => console.log(m),
+    });
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    try {
+      console.log("selectedImage", file);
+      const { data } = await worker.recognize(file);
+      console.log("data", data.text);
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({
+        text: data.text,
+      });
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+      fetch("http://localhost:8000/explain", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          setTextData(result.data);
+        })
+        .catch((error) => console.log("error", error));
+      // setTextResult(data.text);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   const getImageData = (value) => {
-    console.log({ value })
+    console.log({ value });
     let imageUrl = URL.createObjectURL(value);
     setImageData(imageUrl);
-  }
+    convertImageToText(value);
+  };
 
   React.useEffect(() => {
     const explainText = `First, we load the Tesseract core scripts using the worker.load() function.
     After that, we load the language trained model from the cache storage.
     Otherwise, it will download the trained modal from the Tesseract server,
-    cache it, and use it.`
+    cache it, and use it.`;
     setTextData(explainText);
-  })
+  }, []);
 
   return (
     <Box sx={{ m: 2 }}>
       <Stack direction="column" alignItems="center" spacing={2}>
         <Button variant="contained" component="label">
           Upload
-          <input hidden accept="image/*" type="file" onChange={(e) => getImageData(e.target.files[0])} />
+          <input
+            hidden
+            accept="image/*"
+            type="file"
+            onChange={(e) => getImageData(e.target.files[0])}
+          />
         </Button>
-        <Stack sx={{ p: 2, boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)' }}>
-          <Typography variant='h5'>
-            {textData}
-          </Typography>
+        <Stack sx={{ p: 2, boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)" }}>
+          <Typography variant="h5">{textData}</Typography>
         </Stack>
 
-        {imageData &&
-          <Stack sx={{ p: 2, boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)' }}>
+        {imageData && (
+          <Stack sx={{ p: 2, boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)" }}>
             <Box
               component="img"
               sx={{
                 content: `url(${imageData})`,
-                height: { xs: '75vw', sm: '175px', md: '250px', lg: '300px' },
-                maxWidth: '350px',
-                objectFit: 'contain'
+                height: { xs: "75vw", sm: "175px", md: "250px", lg: "300px" },
+                maxWidth: "350px",
+                objectFit: "contain",
               }}
               alt="Logo"
             />
-          </Stack>}
+          </Stack>
+        )}
       </Stack>
     </Box>
-  )
+  );
 }
