6e4ba97c033548c73995c74e8a97b7e2d28922bb
Malhar Lumbhani <malharlumbhani@Malhars-MacBook-Air.local>
2023-03-03 11:37:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+// import Upload from "./components/Upload";
+
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import Upload from './components/Upload';
+
+let theme = createTheme(
+  {
+    typography: {
+      h1: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 'bold',
+      },
+      h2: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 700,
+      },
+      h3: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 'bold',
+      },
+      h4: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 'bold',
+      },
+      h5: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 900,
+      },
+      h6: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 500,
+      },
+      body1: {
+        fontFamily: '"Lato", sans-serif',
+        fontWeight: 400,
+      }
+    },
+  }
+);
+theme = responsiveFontSizes(theme);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      {/* <CssBaseline /> */}
+      <Routes>
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
